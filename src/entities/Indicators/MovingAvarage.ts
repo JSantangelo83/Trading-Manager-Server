ac29a389be801c1e4c96d31edcfe4e38b99f94f7
Joaquin Santangelo <joakinsant@gmail.com>
2021-08-28 23:15:42 -0300

    Moving Avarages fixed decimals

--- src/entities/Indicators/MovingAvarage.ts
+++ src/entities/Indicators/MovingAvarage.ts
@@ -3,7 +3,7 @@ import Candle from "../../interfaces/Candle";
 import IndicatorConfig from "../../interfaces/IndicatorConfig";
 import MovingAvaragesConfig from "../../interfaces/Indicators/MovingAvaragesConfig";
 import Indicator from "../Indicator";
-
+import Helpers from "../../Helpers";
 export default class MovingAvarage extends Indicator {
     constructor(config: MovingAvaragesConfig) {
         let _this: MovingAvaragesConfig = <MovingAvaragesConfig>{};
@@ -17,12 +17,12 @@ export default class MovingAvarage extends Indicator {
             case MovingAvaragesTypes.Simple:
                 _this.formule = (candle: Candle, historicalCandles: Candle[]) => {
                     //TODO Test if any 'candle' hasn't 'source' property. 
-                    return (historicalCandles.slice(historicalCandles.length - _this.period! + 1).map(candle => candle[_this.source!]).reduce((vs, v) => vs! + v!)! + candle[_this.source!]!) / _this.period!
+                    return Helpers.formatFloat((historicalCandles.slice(historicalCandles.length - _this.period! + 1).map(candle => candle[_this.source!]).reduce((vs, v) => vs! + v!)! + candle[_this.source!]!) / _this.period!)
                 }; break;
             case MovingAvaragesTypes.Exponential:
                 _this.formule = (candle: Candle, historicalCandles: Candle[]) => {
                     //TODO Test if any 'candle' hasn't 'source' property. 
-                    return candle[_this.source!]! * (2 / (_this.period! + 1)) + (this.valueArray.length ? this.valueArray[this.valueArray.length - 1] : historicalCandles[historicalCandles.length - 1][_this.source!]!) * (1 - (2 / (_this.period! + 1)))
+                    return Helpers.formatFloat(candle[_this.source!]! * (2 / (_this.period! + 1)) + (this.valueArray.length ? this.valueArray[this.valueArray.length - 1] : historicalCandles[historicalCandles.length - 1][_this.source!]!) * (1 - (2 / (_this.period! + 1))))
                 }; break;
             case MovingAvaragesTypes.Smoothed: break;
             case MovingAvaragesTypes.LinearWeighted: break;
